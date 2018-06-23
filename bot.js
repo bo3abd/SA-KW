82d6ac654281e30e200df47977578fa998bf02f8
bo3abd <37047946+bo3abd@users.noreply.github.com>
2018-06-23 07:33:25 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -147,6 +147,35 @@ client.on('message', message => {
    
 
 
+client.on('message', message => {
+   if(message.content.startsWith(prefix + "invites")) {
+    message.guild.fetchInvites().then(invs => {
+      let user = message.mentions.users.first() || message.author
+      let personalInvites = invs.filter(i => i.inviter.id === user.id);
+      let inviteCount = personalInvites.reduce((p, v) => v.uses + p, 0);
+message.channel.send(`${user} has ${inviteCount} invites.`);
+});
+  }
+});
+
+
+
+
+
+
+client.on('message', message => {
+    if(message.content === '.'){
+        message.channel.send('test')
+    }
+});
+
+
+
+
+
+
+
+
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
