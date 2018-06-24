6eccb5a9b54042aee02ce3e7b21ab2ab7326b814
bo3abd <37047946+bo3abd@users.noreply.github.com>
2018-06-24 03:08:56 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,7 +24,7 @@ client.on('message', message => {
 
 
 const developers = ["415649344864387072","346675179948212225",""]
-const adminprefixe = "^";
+const adminprefix = "^";
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
